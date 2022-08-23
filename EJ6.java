f8182cbd1c26c8b2fe4aedd97ad6f3b001f1f434
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 10:40:27 +0200

    Add files via upload

---
+++ EJ6.java
@@ -0,0 +1,15 @@
+
+public class EJ6 {
+
+	public static void main(String[] args) {
+		short num1=129;
+		System.out.println(num1);
+		int num2=33000;
+		System.out.println(num2);
+		long num3=3000000000l;
+		System.out.println(num3);
+		long num4=23094829l;
+		System.out.println(num4);
+	}
+
+}
