3cff0626368a8f458689d8b57beebe5d9e1bc7cd
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 12:02:42 +0200

    Add files via upload

---
+++ ej9.java
@@ -0,0 +1,16 @@
+package ej9;
+
+public class ej9 {
+
+	public static void main(String[] args) {
+		String nombre="POL";
+		String apellido = "VELA";
+		String calle = "Ctra.matadepera";
+		String municipio = "Terrassa";
+		String codigoPostal = "08225";
+		char sexo = 'H';
+		String correo = "pol@gmil.com";
+		System.out.println(nombre+"\t"+apellido+"\n"+calle+"\n"+municipio+"\n"+codigoPostal+"\n"+sexo+"\n"+correo);
+	}
+
+}
