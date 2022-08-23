531a8a37343b46a61a374d4de4199f50219cef2c
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 12:17:52 +0200

    Add files via upload

---
+++ ej10.java
@@ -0,0 +1,30 @@
+package ej10;
+
+public class ej10 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		int num3 = 3;
+		int num4 = 25;
+		int num5 = 7;
+		int num6 = 4;
+		int num7 = 55;
+		int num8 = 45;
+		int result1 = num1 - num2;
+		System.out.println(result1);
+		int result2 = num7 + num8;
+		System.out.println(result2);
+		int result3 = -num3;
+		System.out.println(result3);
+		int result4 = num2 * num2;
+		System.out.println(result4);
+		int result5 = num4 / num5;
+		System.out.println(result5);
+		float result6 = (float) num4 / num5; 
+		System.out.println(result6);
+		float result7= (float) num4 % num6;
+		System.out.println(result7);
+	}
+
+}
