94fd3ac561eb432b15346605b824415f300c9559
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 10:46:46 +0200

    Add files via upload

---
+++ ej7.java
@@ -0,0 +1,16 @@
+package ej7;
+
+public class ej7 {
+
+	public static void main(String[] args) {
+		final short NUM_1=129;
+		System.out.println(NUM_1);
+		final int NUM_2=33000;
+		System.out.println(NUM_2);
+		final long NUM_3=3000000000l;
+		System.out.println(NUM_3);
+		final long NUM_4=23094829l;
+		System.out.println(NUM_4);
+	}
+
+}
