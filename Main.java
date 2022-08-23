dc34eab5edd13ac25950bc93ec0c0f5e2975fcd6
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 09:08:57 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
-package ej2;
+package ej3;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("I love java");
+		System.out.println("Hello\nWorld");
+		System.out.println("Hello\tWorld");
+		System.out.println("Hello\\World");
+		System.out.println("\"Hello World\"");
+		System.out.println("Hello World\r");
+		System.out.println("Hello\b World");
 	}
 
 }
