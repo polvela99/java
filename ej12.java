c0b80d16133a1e628b65a4828b286cc9efaa04a9
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 12:38:31 +0200

    Add files via upload

---
+++ ej12.java
@@ -0,0 +1,14 @@
+package ej11;
+
+public class ej11 {
+
+	public static void main(String[] args) {		// TODO Auto-generated method stub
+		int num1 = 25;
+		System.out.println(num1+=5);
+		System.out.println(num1-=15);
+		System.out.println(num1*=2);
+		System.out.println(num1/=2);
+		System.out.println(num1%=3);
+	}
+
+}
