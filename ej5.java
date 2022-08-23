ff43e4f7f96df96218c551873628c58fce82d404
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 10:30:34 +0200

    Add files via upload

---
+++ ej5.java
@@ -0,0 +1,23 @@
+package ej5;
+
+public class ej5 {
+
+	public static void main(String[] args) {
+		// Logicos
+		boolean cara=true;
+		// Caracter
+		char nariz = 'A';
+		// Númericos enteros
+		byte boca = 120;
+		// Númericos reales
+		float codo = 1.2329f;
+		// Constantes
+		final String CARA_BOCA = "POL VELA";
+		System.out.println(cara);
+		System.out.println(nariz);
+		System.out.println(boca);
+		System.out.println(codo);
+		System.out.println(CARA_BOCA);
+	}
+
+}
