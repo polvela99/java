225be740f5d63c3b0a8d9343c47fbce94b1a8265
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 12:51:48 +0200

    Add files via upload

---
+++ ej13.java
@@ -0,0 +1,21 @@
+package ej13;
+
+public class ej13 {
+
+	public static void main(String[] args) {
+		int num1=1 , num2=2 , num3=3, num4=4;
+		System.out.println(num4 == num2);
+		System.out.println(num2 == num3);
+		System.out.println(num1 != num3);
+		System.out.println(num2 != num2);
+		System.out.println(num4 < num3);
+		System.out.println(num3 < num1);
+		System.out.println(num1>num3);
+		System.out.println(num2>num4);
+		System.out.println(num2<=num3);
+		System.out.println(num2<=num1);
+		System.out.println(num1>=num2);
+		System.out.println(num2>=num1);
+	}
+
+}
