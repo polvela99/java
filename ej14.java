af31e552b81351c02b874687a098d96f0829b380
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-23 13:19:48 +0200

    Add files via upload

---
+++ ej14.java
@@ -0,0 +1,14 @@
+package ej14;
+
+public class ej14 {
+
+	public static void main(String[] args) {
+		boolean bool1=true;
+		boolean bool2=false;
+		boolean bool3=false;
+		System.out.println(bool1 == !bool2 );
+		System.out.println(bool2 == !bool3);
+
+	}
+
+}
