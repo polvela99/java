3d3a16b750c844a435b0f1ef1e69b4ef0dad9e2e
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-24 11:45:46 +0200

    Add files via upload

---
+++ ej17.java
@@ -0,0 +1,41 @@
+package ej17;
+
+public class ej17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 9;
+		switch(num) {
+		case 1:
+			System.out.println("Uno");
+			break;
+		case 2:
+			System.out.println("dos");
+			break;
+		case 3:
+			System.out.println("tres");
+			break;
+		case 4:
+			System.out.println("cuatro");
+			break;
+		case 5:
+			System.out.println("cinco");
+			break;
+		case 6:
+			System.out.println("seis");
+			break;
+		case 7:
+			System.out.println("siete");
+			break;
+		case 8:
+			System.out.println("ocho");
+			break;
+		case 9:
+			System.out.println("nueve");
+			break;
+		}
+		double nume = 0;
+		System.out.println(nume>0?"Es positivo":nume==0?"Na de na":"Es negativo");
+	}
+
+}
