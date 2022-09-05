30834ae31b9e6d38bb35fc9a14dc35b63df04b4d
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-09-05 10:09:41 +0200

    Add files via upload

---
+++ ej28.java
@@ -0,0 +1,15 @@
+package ej28;
+
+public class ej28 {
+
+	public static void main(String[] args) {
+		System.out.printf("%s\t\t\t%s\t%s%n","NOMBRE","APELLIDO1","APELLIDO2");
+		System.out.printf("%s  %s    %s%n","NOMBRE","APELLIDO1","APELLIDO2");
+		System.out.printf("%3$s, %2$s , %1$s%n","NOMBRE","APELLIDO1","APELLIDO2");
+		System.out.printf("%s             %d%n","", 22);
+		System.out.printf("%010d%n", 22);
+		System.out.printf("%09.2f%n", 17.181932);
+		System.out.printf("%s%s","Hola","Mundo");
+	}
+
+}
